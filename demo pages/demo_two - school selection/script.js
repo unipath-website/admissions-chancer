3953b0426207f65a2b6863a202949c045047e146
Soroush Paidar <soroush.paidar@gmail.com>
2024-08-12 11:56:17 -0400

    081224 - 11:56

--- demo pages/demo_two - school selection/script.js
+++ demo pages/demo_two - school selection/script.js
@@ -1,40 +1,30 @@
-function schoolSelection() {
-    let mean;
-    let stdev;
+let mean;
+let stdev;
 
-    const dropdown = document.getElementById('schoolSelect');
-    const resultParagraph = document.getElementById('result');
+const dropdown = document.getElementById('schoolSelect');
+const resultParagraph = document.getElementById('result');
 
-    function updateValues() {
-        const selectedValue = dropdown.value;
+function schoolStatistics() {
+    const selectedValue = dropdown.value;
 
-        switch (selectedValue) {
-            case 'utsg':
-                mean = 96.9;
-                stdev = 3.05;
-                break;
-            case 'mac':
-                mean = 96.9;
-                stdev = 2.47;
-                break;
-
-            default:
-                mean = 0;
-                stdev = 0;
-                break;
-        }
-
-
-        console.log('Mean:', mean);
-        console.log('Standard Deviation:', stdev);
-        resultParagraph.textContent = `Mean: ${mean}, Standard Deviation: ${stdev}`;
+    switch (selectedValue) {
+        case 'utsg':
+            mean = 96.9;
+            stdev = 3.05;
+            break;
+        case 'mac':
+            mean = 96.9;
+            stdev = 2.47;
+            break;
+        default:
+            mean = 0;
+            stdev = 1;
+            break;
     }
 
-
-
-
-// Initialize values on page load
-
+    resultParagraph.textContent = `Mean: ${mean}, Standard Deviation: ${stdev}`;
 }
-dropdown.addEventListener('change', updateValues);
-updateValues();
+
+dropdown.addEventListener('change', schoolStatistics);
+
+schoolStatistics();
