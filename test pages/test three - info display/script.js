1024529e5fc5196cb0a8421cdce20644b241a15d
Soroush Paidar <84630919+soroush-paidar@users.noreply.github.com>
2024-08-17 16:00:23 -0400

    starting to work on home page (!!)

--- test pages/test three - info display/script.js
+++ test pages/test three - info display/script.js
@@ -1,3 +1,5 @@
+let name;
+let schoolloc;
 function changeImage() {
     const imageSelector = document.getElementById("imageSelector").value;
     const imageElement = document.getElementById("displayedImage");
