d0d1b5d0c1f9b78683b404a65ec1fc77c040e513
Soroush Paidar <84630919+soroush-paidar@users.noreply.github.com>
2024-09-08 17:29:24 -0400

    9/8 1729

--- test new/script.js
+++ test new/script.js
@@ -1,4 +1,4 @@
-let mean;
+let kmd;
 let stdev;
 let schoolloc;
 
@@ -13,12 +13,12 @@ const schoolPrograms = {
         name: "University of Toronto",
         location: "Toronto, ON",
         programs: {
-            "Social Sciences (HBA)": {mean: 91.8, stdev: 4.65},
-            "Physical and Mathematical Sciences (HBSc)": {mean: 93.4, stdev: 4.112},
-            "Humanities (HBA)": {mean: 91.5, stdev: 5.1},
-            "Life Sciences (HBSc)": {mean: 94.1, stdev: 4.08},
-            "Rotman Commerce (BCom)": {mean: 95.2, stdev: 3.248},
-            "Computer Science (HBSc)": {mean: 96.9, stdev: 3.05},
+            "Social Sciences": {kmd: 91.8, stdev: 4.65},
+            "Physical and Mathematical Sciences": {kmd: 93.4, stdev: 4.112},
+            "Humanities": {kmd: 91.5, stdev: 5.1},
+            "Life Sciences": {kmd: 94.1, stdev: 4.08},
+            "Rotman Commerce": {kmd: 95.2, stdev: 3.248},
+            "Computer Science": {kmd: 96.9, stdev: 3.05},
         },
         image: "./logo/UniversityOfToronto.jpg"
     },
@@ -26,9 +26,12 @@ const schoolPrograms = {
         name: "University of Waterloo",
         location: "Waterloo, ON",
         programs: {
-            "Computer Science": { mean: 97, stdev: 3.0 },
-            "Engineering": { mean: 96, stdev: 2.8 },
-            "Mathematics": { mean: 94, stdev: 4.0 }
+            "School of Architecture": {kmd: 91.3, stdev: 4.77},
+            "School of Computer Science": {kmd: 97.8, stdev: 3.9},
+            "Faculty of Engineering": {kmd: 95.6, stdev: 3.43},
+            "Faculty of Mathematics": {kmd: 95.1, stdev: 3.37},
+            "Faculty of Health": {kmd: 90.7, stdev: 3.82},
+            "Faculty of Science": {kmd: 91.4, stdev: 4.91},
         },
         image: "./logo/UniversityOfWaterloo.svg"
     },
@@ -36,11 +39,30 @@ const schoolPrograms = {
         name: "McMaster University",
         location: "Hamilton, ON",
         programs: {
-            "Health Sciences": { mean: 98, stdev: 2.0 },
-            "Engineering": { mean: 92, stdev: 3.8 },
-            "Business": { mean: 90, stdev: 4.2 }
+            "Health Sciences": {kmd: 98, stdev: 2.0},
+            "Engineering": {kmd: 92, stdev: 3.8},
+            "Business": {kmd: 90, stdev: 4.2}
         },
         image: "./logo/McMaster_University_Logo.svg"
+    },
+    qu: {
+        name: "Queen's University",
+        location: "Kingston, ON",
+        programs: {
+            "Life Sciences and Biochemistry": {kmd: 94.5, stdev: 3.36},
+            "Commerce": {kmd: 94.5, stdev: 3.73},
+            "Computing": {kmd: 93.3, stdev: 3.96},
+            "Concurrent Education": {kmd: 93.1, stdev: 4.59},
+            "Engineering and Applied Science": {kmd: 93.5, stdev: 3.8},
+            "Health Sciences": {kmd: 96.8, stdev: 3.7},
+            "Kinesiology": {kmd: 92.7, stdev: 3.69},
+            "Arts": {kmd: 89.7, stdev: 5.03},
+            "Music/Theatre": {kmd: 90.8, stdev: 8},
+            "Nursing": {kmd: 94.5, stdev: 3.48},
+            "Psychology": {kmd: 85.3, stdev: 6.2},
+            "Science": {kmd: 90.5, stdev: 4.88},
+        },
+        image: "./logo/QueensU.svg"
     }
 };
 
@@ -83,18 +105,18 @@ function calculateProb() {
     const selectedProgram = programDropdown.value;
 
     if (schoolPrograms[selectedSchool] && schoolPrograms[selectedSchool].programs[selectedProgram]) {
-        mean = schoolPrograms[selectedSchool].programs[selectedProgram].mean;
+        kmd = schoolPrograms[selectedSchool].programs[selectedProgram].kmd;
         stdev = schoolPrograms[selectedSchool].programs[selectedProgram].stdev;
         schoolloc = schoolPrograms[selectedSchool].location;
         imageElement.src = schoolPrograms[selectedSchool].image;
     } else {
-        mean = 0;
+        kmd = 0;
         stdev = 1;
     }
 
     let sum = numbers.reduce((acc, num) => acc + num, 0);
     let top6 = sum / numbers.length;
-    let zscnum = top6 - mean;
+    let zscnum = top6 - kmd;
     let zscore = zscnum / (Math.sqrt(2) * stdev);
     let probabilitor = 100 * (0.5 + 0.5 * (math.erf(zscore)));
 
@@ -105,3 +127,5 @@ function calculateProb() {
 schoolDropdown.addEventListener('change', populatePrograms);
 
 populatePrograms();
+
+/* made w love by soroush paidar */
